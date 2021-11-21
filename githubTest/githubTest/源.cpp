52d78cd78abb9799ddf971922d847fe477688dba
GithubLyb2021 <2639638289@qq.com>
2021-11-21 20:27:33 +0800

    函数重载

--- githubTest/githubTest/源.cpp
+++ githubTest/githubTest/源.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <Windows.h>
 
+int add(int a,int b) {
+	std::cout << "function 1" << std::endl;
+	std::cout << a + b << std::endl;
+	return a + b;
+}
+
+int add(int a, int b, int c = 1) {  
+	std::cout << "function 2" << std::endl;
+	std::cout << a + b + c << std::endl;
+	return a + b;
+}
+
 int main(void) {
 	std::cout << "hello world!" << std::endl;
+	//add(1, 2);  //“add” : 对重载函数的调用不明确
+	add(1, 2, 3);
 	system("pause");
 	return 0;
 }
